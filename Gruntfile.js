6295f4c51cfbfccf565a1ae911e659dcf1c64274
Doug Martin <doug@zoopdoop.com>
2013-03-18 20:03:17 -0400

    initial checkin

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/*
+ * zoopbake
+ * https://github.com/vagrant/gruntplugin
+ *
+ * Copyright (c) 2013 Zoopdoop, LLC
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // By default, lint
+  grunt.registerTask('default', ['jshint']);
+
+};
---
+++ tasks/zoopbake.js
@@ -0,0 +1,57 @@
+/*
+ * zoopbake
+ * https://github.com/vagrant/gruntplugin
+ *
+ * Copyright (c) 2013 Zoopdoop, LLC
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  var zoopbake = require('zoopbake/lib/zoopbake.js').zoopbake;
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('zoopbake', 'Your task description goes here.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      verbose: false,
+      ignoreErrors: true,
+      autoEscape: true,
+      encoding: 'utf8',
+      tzOffset: 0,
+      sourceRoot: '',
+      force: false
+    }), 
+    done;
+    
+    // make sure there is only one file group
+    if ((this.files.length === 0) || (this.files[0].src.length === 0)) {
+      grunt.log.error('No src file directive found');
+      return false;
+    }
+    if ((this.files.length > 1) || (this.files[0].src.length > 1)) {
+      grunt.log.error('Only one src file directive is allowed');
+      return false;
+    }
+    if (this.files[0].src.length === 0) {
+      grunt.log.error('Empty src file directive');
+      return false;
+    }    
+    if (this.files[0].dest.length === 0) {
+      grunt.log.error('Empty dest file directive');
+      return false;
+    }    
+    
+    // let grunt know that zoopbake is async
+    done = this.async();
+    
+    return zoopbake(this.files[0].src[0], this.files[0].dest, options, function (err, success) {
+      done();
+    });
+  });
+
+};
